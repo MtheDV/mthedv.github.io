756af0d2f8bdb5822ee8c0ad25ce85bd8cef13e2
Mathew de Vin <mathewdevin03@gmail.com>
2019-12-20 01:24:12 -0800

    Implemented logo in top left of website

---
+++ scripts.js
@@ -0,0 +1,10 @@
+/* scripts for use in website
+ * created by mathew de vin
+ *
+ */
+
+let logoTopLeft = document.querySelector(".logoTopLeft");
+
+logoTopLeft.onclick = function() {
+    window.scrollTo({top: 0, behavior: "smooth"});
+}
