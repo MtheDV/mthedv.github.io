a822142154bd2c1e2508935b7b1694ce6629065e
Mathew de Vin <mathewdevin03@gmail.com>
2019-12-20 17:49:47 -0800

    Changed works/contact jumps to js

--- scripts.js
+++ scripts.js
@@ -3,7 +3,17 @@
  */
 
 let logoTopLeft = document.querySelector(".logoTopLeft");
+let contactButton = document.getElementById("contactButton");
+let worksButton = document.getElementById("worksButton");
 
 logoTopLeft.onclick = function() {
     window.scrollTo({top: 0, behavior: "smooth"});
+}
+
+contactButton.onclick = function() {
+    document.getElementById("contactLinks").scrollIntoView({behavior: "smooth"});
+}
+
+worksButton.onclick = function() {
+    document.getElementById("worksGallery").scrollIntoView({behavior: "smooth"});
 }
