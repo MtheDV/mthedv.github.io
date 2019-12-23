530b1f111e9c96122284cfc1f479ea935510826d
Mathew de Vin <mathewdevin03@gmail.com>
2019-12-23 02:09:07 -0800

    Finished the design of the website. Added portfolio examples

--- scripts.js
+++ scripts.js
@@ -5,15 +5,47 @@
 let logoTopLeft = document.querySelector(".logoTopLeft");
 let contactButton = document.getElementById("contactButton");
 let worksButton = document.getElementById("worksButton");
+let experimentButton = document.getElementById("experimentButton");
+let gallery = document.querySelectorAll(".gallery");
 
+// buttons that direct the window around the page
+// top of page logo button
 logoTopLeft.onclick = function() {
     window.scrollTo({top: 0, behavior: "smooth"});
 }
 
+// contacts / bottom of page
 contactButton.onclick = function() {
     document.getElementById("contactLinks").scrollIntoView({behavior: "smooth"});
 }
 
+// works section of the page
 worksButton.onclick = function() {
     document.getElementById("worksGallery").scrollIntoView({behavior: "smooth"});
 }
+
+// exiremental section of the page
+experimentButton.onclick = function() {
+    document.getElementById("experimentalGallery").scrollIntoView({behavior: "smooth"})
+}
+
+// when scrolling past the logo ~200px, the top left logo appears
+window.addEventListener("scroll", function() {
+    if (this.window.pageYOffset < 200) {
+        logoTopLeft.style.opacity = 0.0;
+    } else {
+        logoTopLeft.style.opacity = 1.0;
+    }
+}, false);
+
+//canvas editing
+/*let mainCanvas = document.querySelector(".mainCanvas");
+let canvas = mainCanvas.getContext("2d");
+
+mainCanvas.width = window.innerWidth * 2;
+mainCanvas.height = window.innerHeight * 2;
+
+canvas.moveTo(0, 0);
+canvas.lineTo(200, 100);
+canvas.stroke();*/
+
