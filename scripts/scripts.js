0b5f4fea4cb4ad2866acc90ca285d1dd3b398b52
Mathew de Vin <mathewdevin03@gmail.com>
2020-03-30 21:05:17 -0700

    added contacts and modified projects page

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,9 +5,18 @@
 ///////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
 
-let backgroundParrCircle = document.querySelector(".background_circle");
-let backgroundParrText = document.querySelector(".background_text");
-let backgroundParrMountains = document.querySelector(".background_mountains");
+let backgroundParrCircleBlue = document.querySelector("#blue_circle");
+let backgroundParrTextHome = document.querySelector("#home_text");
+let backgroundParrMountains = document.querySelector("#background_mountains");
+let backgroundParrCircleOrange = document.querySelector("#orange_circle");
+let backgroundParrTextContact = document.querySelector("#contact_text");
+let backgroundParrClouds = document.querySelector("#background_clouds");
+/* pageTypes
+ * 0 = home
+ * 1 = contacts
+ * 2 = projects
+ */
+let pageType = 0;
 
 function parallaxCalculate(distance, mouse, speed) {
     return (((distance - mouse) / distance * 100) - 50) * speed;
@@ -28,17 +37,43 @@ function parallax(e) {
     let topMountain = parallaxCalculate(height, mouseY, 0.08);
     let leftMountain = parallaxCalculate(width, mouseX, 0.1);
 
-    backgroundParrCircle.style.top = topCircle + "%";
-    backgroundParrCircle.style.left = leftCircle + "%";
-    backgroundParrText.style.top = topText + 37 + "%";
-    backgroundParrText.style.left = leftText + "%";
-    backgroundParrMountains.style.top = topMountain + 50 + "%";
-    backgroundParrMountains.style.left = leftMountain + "%";
+    if (pageType == 0) {
+        backgroundParrCircleBlue.style.top = topCircle + "%";
+        backgroundParrCircleBlue.style.left = leftCircle + "%";
+        backgroundParrTextHome.style.top = topText + 37 + "%";
+        backgroundParrTextHome.style.left = leftText + "%";
+        backgroundParrMountains.style.top = topMountain + 50 + "%";
+        backgroundParrMountains.style.left = leftMountain + "%";
+    } else if (pageType == 1) {
+        backgroundParrCircleOrange.style.top = topCircle + "%";
+        backgroundParrCircleOrange.style.left = leftCircle + "%";
+        backgroundParrTextContact.style.top = topText + 25 + "%";
+        backgroundParrTextContact.style.left = leftText - 35 + "%";
+        backgroundParrClouds.style.top = topMountain + 30 + "%";
+        backgroundParrClouds.style.left = leftMountain + 15 + "%";
+    }
 }
 window.addEventListener("mousemove", parallax);
 
-let parallaxBackground = document.querySelector(".parallax_background");
-let gallery = document.querySelector(".gallery");
+let wholeGallery = document.querySelector(".gallery");
+let galleryX = 0;
+
+function moveGallery(e) {
+    let width = window.innerWidth;
+    let mouseX = e.clientX;
+
+    if (mouseX > width - 400)
+        galleryX -= 15;
+    else if (mouseX < 400)
+        galleryX += 15;
+
+    if (galleryX >= 5)
+        galleryX = 5;
+
+    wholeGallery.style.left = galleryX + "%";
+}
+window.addEventListener("mousedown", moveGallery);
+
 
 function animateOut(element) {
     element.style.opacity = "0.0";
@@ -51,15 +86,46 @@ function animateIn(element) {
     element.style.display = "block";
 }
 
+let parallaxBackgroundHome = document.querySelector(".parallax_background_home");
+let parallaxBackgroundContact = document.querySelector(".parallax_background_contact");
+let projects = document.querySelector(".gallery");
+
 let projectsButton = document.querySelector(".projects_button");
 let logoButton = document.querySelector(".logo");
+let contactButton = document.querySelector(".contact_button");
+let mailButton = document.querySelector(".mail_button");
+let linkedinButton = document.querySelector(".linkedin_button");
+let instagramButton = document.querySelector(".instagram_button");
 
 projectsButton.onclick = function() {
-    animateOut(parallaxBackground);
-    animateIn(gallery);
+    animateOut(parallaxBackgroundHome);
+    animateOut(parallaxBackgroundContact);
+
+    animateIn(projects);
+    pageType = 2;
 }
 logoButton.onclick = function() {
-    animateOut(gallery);
-    animateIn(parallaxBackground);
+    animateOut(projects);
+    animateOut(parallaxBackgroundContact);
+
+    animateIn(parallaxBackgroundHome);
+    pageType = 0;
+}
+contactButton.onclick = function() {
+    animateOut(projects);
+    animateOut(parallaxBackgroundHome);
+
+    animateIn(parallaxBackgroundContact);
+    pageType = 1;
+}
+
+mailButton.onclick = function() {
+    window.open("mailto:mathewdevin03@gmail.com");
+}
+linkedinButton.onclick = function() {
+    window.open("https://www.linkedin.com/in/mathew-de-vin-705a8a198/");
+}
+instagramButton.onclick = function() {
+    window.open("https://www.instagram.com/mathew_dv/");
 }
 
